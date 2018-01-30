b2bf23d603a5ca288375ee45ed50b7e63deadd3f
Pankaj <pegulhane@hotmail.com>
2018-01-29 22:26:37 -0800

    Working linear regression code

--- tensorflow/basic/LR/linear_regrssion.py
+++ tensorflow/basic/LR/linear_regrssion.py
@@ -1,25 +1,44 @@
 import tensorflow as tf
-
+import numpy as np
 
 class Model:
-  def create_model(feature_dim):
+  def create_model(self, feature_dim):
   # batch x features
     self.x = tf.placeholder(tf.float32, shape=[None, feature_dim], name="X")
-    self.y = tf.placeholder(tf.int, shape=[None, 1], name="Y")
+    self.y = tf.placeholder(tf.float32, shape=[None, 1], name="Y")
 
     W = tf.get_variable("W", shape=[feature_dim, 1])
-    b = tf.get_variable("b", shape=[None, 1])
+    b = tf.get_variable("b", shape=[1, 1])
 
-    y_pred = tf.matmul(X,W) + b
+    y_pred = tf.matmul(self.x, W) + b
 
     # Compute loss, divinding by 2 just for loss gradient computation
-    self.loss = tf.reduce_mean(tf.pow((y-y_pred), 2))/2
+    self.loss = tf.reduce_mean(tf.pow((self.y-y_pred), 2))/2
 
     self.optimiser = tf.train.GradientDescentOptimizer(0.01).minimize(self.loss)
 
-    self.init = tf.global_variable_intializer()
-    
+    self.init = tf.global_variables_initializer()
 
-  def train():
+  def run_test(self, batch_size):
+    train_X = np.asmatrix([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
+                              7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1, 3.1]).reshape(-1, batch_size)
+    train_Y = np.asmatrix([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
+                              2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3, 1.3]).reshape(-1, batch_size)
+    self.train(train_X, train_Y)
+
+
+  def train(self, train_x, train_y):
+    with tf.Session() as session:
+      session.run(self.init)
+      for i in range(50):
+        for (x, y) in zip(train_x, train_y):
+          pred_loss, _ = session.run([self.loss, self.optimiser],
+                                     feed_dict={self.x: np.transpose(x),
+                                                self.y: np.transpose(y)})
+
+        print(pred_loss)
     
-    
+if __name__ == '__main__':
+  model = Model()
+  model.create_model(1)
+  model.run_test(2)
