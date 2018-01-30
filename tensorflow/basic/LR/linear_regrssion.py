ba63ded51bfea912980375c727b60a585a423e9e
Pankaj <pegulhane@hotmail.com>
2018-01-29 23:07:39 -0800

    Working linear regression code

--- tensorflow/basic/LR/linear_regrssion.py
+++ tensorflow/basic/LR/linear_regrssion.py
@@ -3,7 +3,8 @@ import numpy as np
 
 class Model:
   def create_model(self, feature_dim):
-  # batch x features
+    self.feature_dim = feature_dim
+    # batch x features
     self.x = tf.placeholder(tf.float32, shape=[None, feature_dim], name="X")
     self.y = tf.placeholder(tf.float32, shape=[None, 1], name="Y")
 
@@ -20,10 +21,11 @@ class Model:
     self.init = tf.global_variables_initializer()
 
   def run_test(self, batch_size):
-    train_X = np.asmatrix([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
-                              7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1, 3.1]).reshape(-1, batch_size)
-    train_Y = np.asmatrix([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
-                              2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3, 1.3]).reshape(-1, batch_size)
+    train_X = np.asarray([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
+                              7.042, 10.791, 5.313, 7.997, 5.654, 9.27]).reshape(-1, batch_size, self.feature_dim)
+    train_Y = np.asarray([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
+                              2.827, 3.465, 1.65, 2.904, 2.42, 2.94])[:train_X.shape[0]*batch_size].reshape(-1, batch_size,1)
+
     self.train(train_X, train_Y)
 
 
@@ -32,13 +34,13 @@ class Model:
       session.run(self.init)
       for i in range(50):
         for (x, y) in zip(train_x, train_y):
-          pred_loss, _ = session.run([self.loss, self.optimiser],
-                                     feed_dict={self.x: np.transpose(x),
-                                                self.y: np.transpose(y)})
+            pred_loss, _ = session.run([self.loss, self.optimiser],
+                                       feed_dict={self.x: x,
+                                                  self.y: y})
 
         print(pred_loss)
     
 if __name__ == '__main__':
   model = Model()
-  model.create_model(1)
+  model.create_model(2)
   model.run_test(2)
