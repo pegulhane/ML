d432618ddf97d9b5eb9ff0fe6b748ddcacc11479
Pankaj <pegulhane@hotmail.com>
2018-01-31 23:06:59 -0800

    Added Signoid for Logistic regression

--- tensorflow/basic/LR/logistic_regression.py
+++ tensorflow/basic/LR/logistic_regression.py
@@ -7,15 +7,15 @@ class Model:
 
         # batch x features
         self.x = tf.placeholder(tf.float32, shape=[None, feature_dim], name="X")
-        self.y = tf.placeholder(tf.int, shape=[None, 1], name="Y")
+        self.y = tf.placeholder(tf.float32, shape=[None, 1], name="Y")
 
         W = tf.get_variable("W", shape=[feature_dim, 1])
         b = tf.get_variable("b", shape=[1, 1])
 
-        y_pred = tf.matmul(self.x, W) + b
+        y_pred = tf.sigmoid(tf.matmul(self.x, W) + b)
 
         # Compute loss, divinding by 2 just for loss gradient computation
-        self.loss = tf.reduce_mean(tf.pow((self.y - y_pred), 2)) / 2
+        self.loss = tf.reduce_mean( - self.y * tf.log(y_pred), axis=0)
 
         self.optimiser = tf.train.GradientDescentOptimizer(0.01).minimize(self.loss)
 
@@ -24,8 +24,8 @@ class Model:
     def run_test(self, batch_size):
         train_X = np.asarray([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167,
                               7.042, 10.791, 5.313, 7.997, 5.654, 9.27]).reshape(-1, batch_size, self.feature_dim)
-        train_Y = np.asarray([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
-                              2.827, 3.465, 1.65, 2.904, 2.42, 2.94])[:train_X.shape[0] * batch_size].reshape(-1,
+        train_Y = np.asarray([0 if x < 2.5 else 1  for x in [1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221,
+                              2.827, 3.465, 1.65, 2.904, 2.42, 2.94]])[:train_X.shape[0] * batch_size].reshape(-1,
                                                                                                               batch_size,
                                                                                                               1)
 
